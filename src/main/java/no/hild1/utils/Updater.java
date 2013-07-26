a7c072c6a8ba663a771451c06a30a4253ddbd09a
Anders Einar Hilden <hildenae@gmail.com>
2013-07-26 17:38:02 +0200

    Why will this not work!!

--- src/main/java/no/hild1/utils/Updater.java
+++ src/main/java/no/hild1/utils/Updater.java
@@ -1,40 +1,28 @@
 package no.hild1.utils;
 
-import org.json.simple.*;
-import org.json.simple.parser.*;
+import java.net.*;
+import java.io.*;
 import java.util.*;
+import org.json.JSONObject;
 
 public class Updater {
     public String foo;
     public Updater(String f) {
         this.foo = f;
     }
-    public static void main(String[] args) {
-        Updater upd = new Updater("Hello World");
-        System.out.println(upd.foo);
-        String jsonText = "{\"first\": 123, \"second\": [4, 5, 6], \"third\": 789}";
-            JSONParser parser = new JSONParser();
-            ContainerFactory containerFactory = new ContainerFactory(){
-                public List creatArrayContainer() {
-                    return new LinkedList();
-                }
-                public Map createObjectContainer() {
-                    return new LinkedHashMap();
-                }
-            };
-        try {
-            Map json = (Map)parser.parse(jsonText, containerFactory);
-            Iterator iter = json.entrySet().iterator();
-            System.out.println("==iterate result==");
-            while(iter.hasNext()){
-                Map.Entry entry = (Map.Entry)iter.next();
-                System.out.println(entry.getKey() + "=>" + entry.getValue());
-            }
-            System.out.println("==toJSONString()==");
-            System.out.println(JSONValue.toJSONString(json));
-        }
-        catch(ParseException pe){
-            System.out.println(pe);
-        }
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://hjelp-meg.nu.hild1.no:8080/job/telepie/lastSuccessfulBuild/no.hild1.bank$telepie/api/json");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+        String jsonText = "";
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            jsonText += inputLine; //System.out.println(inputLine);
+        in.close();
+    
+        //Updater upd = new Updater("Hello World");
+        //System.out.println(upd.foo);
+        JSONObject json = JSONObject(jsonText);
     } 
+
 } 
